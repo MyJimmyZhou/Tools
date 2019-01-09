eec29076f015ac3db4869ad54707f786bacfcf56
继良 周 <zjl_hnyz@163.com>
2019-01-09 21:58:33 +0800

    添加项目文件。

---
+++ Entiry/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Entiry
+{
+    public class Student
+    {
+        public string Name { set; get; }
+        public int Age { set; get; }
+        public double Height { set; get; }
+    }
+}
---
+++ ExpressionExTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Entiry;
+using LinqExtend;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpressionExTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
